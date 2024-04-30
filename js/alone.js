9cd8e6010d439602694fe2e6aa70e402ee66f362
ZAWARUNO <147724643+ZAWARUNO@users.noreply.github.com>
2024-04-30 21:38:39 +0700

    Add files via upload

---
+++ js/alone.js
@@ -0,0 +1,6 @@
+document.getElementById("search-button").addEventListener("click", function() {
+    var searchTerm = document.getElementById("search-bar").value;
+    // Lakukan sesuatu dengan searchTerm, seperti mengirim permintaan pencarian
+    // atau menampilkan hasil pencarian.
+  });
+  
